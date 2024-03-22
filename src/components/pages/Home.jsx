bd930297a0f4714f4bb44f7c8f702d025726346b
nanawaredivya <divyananaware28@gmail.com>
2024-03-22 13:17:17 +0530

    connect database

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,39 +1,20 @@
-import React from 'react'
+import React from 'react';
 import { Container, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 import HeaderImg from './HeaderImg';
 import { HeaderCard } from './HeaderCard';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  appBar: {
-    marginBottom: 3,
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-}));
-
 const Home = () => {
-    const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <div style={{ flexGrow: 1 }}>
       <Container>
-      <br/>
-      <HeaderImg/>
-      <br/><br/>
-      <Typography variant='h4' color={'orange'} align='center'>"Take care of your body. It's the only place you have to live in."</Typography>
-      <br/><br/>
-      <HeaderCard/> 
-      <br/>
+        <HeaderImg />
+        <Typography variant='h4' style={{ color: 'orange', textAlign: 'center', margin: '20px 0' }}>
+          "Take care of your body. It's the only place you have to live in."
+        </Typography>
+        <HeaderCard />
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,19 +1,39 @@
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getFirestore } from "firebase/firestore";
+
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBhlmK8_2RC_O1Z2D8Aachrv4kRe_H6RHQ",
+//     authDomain: "portfolio-contact-from.firebaseapp.com",
+//     projectId: "portfolio-contact-from",
+//     storageBucket: "portfolio-contact-from.appspot.com",
+//     messagingSenderId: "819359560589",
+//     appId: "1:819359560589:web:12be579237c6d4831943c4",
+//     measurementId: "G-BRMVMBW1VX"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app);
+// export const db = getFirestore(app);
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBhlmK8_2RC_O1Z2D8Aachrv4kRe_H6RHQ",
-    authDomain: "portfolio-contact-from.firebaseapp.com",
-    projectId: "portfolio-contact-from",
-    storageBucket: "portfolio-contact-from.appspot.com",
-    messagingSenderId: "819359560589",
-    appId: "1:819359560589:web:12be579237c6d4831943c4",
-    measurementId: "G-BRMVMBW1VX"
+  apiKey: "AIzaSyD5sPCK_S3dtP8_AMX083Vt6cv5fFS4YrU",
+  authDomain: "shree-clinic-61a21.firebaseapp.com",
+  projectId: "shree-clinic-61a21",
+  storageBucket: "shree-clinic-61a21.appspot.com",
+  messagingSenderId: "433189446845",
+  appId: "1:433189446845:web:5ae40259baafdae00f43d4",
+  measurementId: "G-VHKZ3RZVYE"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
